7eefcb176f2eb0c9f59747522b4f9e6aefb3708c
= <=>
2019-11-21 14:08:44 +0700

    fix route booking

--- src/components/dashboardUserLayout.vue
+++ src/components/dashboardUserLayout.vue
@@ -11,7 +11,7 @@
       <v-toolbar-items>
         <v-btn text router to="/user/home">Home</v-btn>
         <v-btn text router to="/user/contactus">Contact Us</v-btn>
-        <v-btn text router to="/users/booking">Booking</v-btn>
+        <v-btn text router to="/user/booking">Booking</v-btn>
         <v-menu
           v-model="menu"
           :close-on-content-click="true"
